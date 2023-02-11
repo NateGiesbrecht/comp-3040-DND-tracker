b9f43fded7ca29e452c43c3fcbad511f93d16754
Nathan Giesbrecht <giesbr93@myumanitoba.ca>
2023-02-11 14:18:29 -0600

    Install prettier

--- dndtracker/src/App.js
+++ dndtracker/src/App.js
@@ -3,20 +3,18 @@ import './App.css';
 import Home from './Home';
 import PlayerHome from './Player/PlayerHome';
 import DMHome from './DM/DMHome';
-import {BrowserRouter as Router} from "react-router-dom"
-import {Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route exact path='/' element={<Home/>} /> 
-        <Route exact path='/Player' element={<PlayerHome/>} /> 
-        <Route exact path='/DM' element={<DMHome/>} /> 
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/Player" element={<PlayerHome />} />
+        <Route exact path="/DM" element={<DMHome />} />
       </Routes>
     </>
-    
-  
   );
 }
 
--- dndtracker/src/DM/DMHome.js
+++ dndtracker/src/DM/DMHome.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from 'react';
 import { Typography, Button } from '@mui/material';
-import {Link} from "react-router-dom"
+import { Link } from 'react-router-dom';
 
 const DMHome = () => {
   return (
     <>
-    <Typography variant="h1" align="center">DM Home</Typography>
-    <Button  component={Link} to={'/'} variant="outlined">Home</Button>
+      <Typography variant="h1" align="center">
+        DM Home
+      </Typography>
+
+      <Button component={Link} to={'/'} variant="outlined">
+        Home
+      </Button>
     </>
-  )
-}
+  );
+};
 
 export default DMHome;
