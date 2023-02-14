c956773625695c03a0b57b5e03134a9ce90ca929
Nathan Giesbrecht <giesbr93@myumanitoba.ca>
2023-02-14 12:51:33 -0600

    Character cards

--- dndtracker/src/Player/PlayerHome.js
+++ dndtracker/src/Player/PlayerHome.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { Button, Typography, Container } from '@mui/material';
+// import { Link } from 'react-router-dom';
+// import PlayerNotes from './PlayerNotes';
+import CharacterCards from './CharacterCards';
 import { Link } from 'react-router-dom';
-import PlayerNotes from './PlayerNotes';
+
+const characters = [
+  { name: 'Fritz', charClass: 'Monk' },
+  { name: 'Squash', charClass: 'Barbarian' },
+  { name: 'Alyia', charClass: 'Cleric' },
+  { name: 'Carcis', charClass: 'Warlock' },
+];
 
 const PlayerHome = () => {
   return (
     <>
       <Typography variant="h1" align="center">
-        Player Home
+        Characters
       </Typography>
       <Container>
-        <Button component={Link} to={'/'} variant="outlined">
+        <Button
+          component={Link}
+          to={'/'}
+          variant="outlined"
+          sx={{ marginRight: '2%' }}
+        >
           Home
         </Button>
-        <PlayerNotes />
+        <Button variant="outlined" sx={{ marginTop: '2%', marginBottom: '2%' }}>
+          Create New Character
+        </Button>
+        <CharacterCards characters={characters} />
       </Container>
     </>
   );
