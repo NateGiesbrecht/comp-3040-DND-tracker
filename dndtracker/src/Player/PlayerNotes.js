3249b9ec39dec7fdf8f4670b4b6248b4b526cb81
Nathan Giesbrecht <giesbr93@myumanitoba.ca>
2023-02-16 16:35:49 -0600

    Add list and changeable notes

--- dndtracker/src/Player/PlayerNotes.js
+++ dndtracker/src/Player/PlayerNotes.js
@@ -1,14 +1,29 @@
 import React, { useState } from 'react';
-import { Typography, Container, TextField, Button } from '@mui/material';
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  Container,
+  TextField,
+  Button,
+  Grid,
+} from '@mui/material';
 import { Link, useParams } from 'react-router-dom';
 
 import { characters } from '../Data/Characters';
 
 export const PlayerNotes = () => {
   const [note, setNote] = useState('');
+  const [index, setIndex] = useState(0);
+
+  const handleItemClick = (content, index) => {
+    setNote(content);
+    setIndex(index);
+  };
 
   const handleSave = () => {
-    console.log(note); // or do something else with the note
+    character.notes[index].content = note; // or do something else with the note
   };
 
   const params = useParams();
@@ -30,23 +45,50 @@ export const PlayerNotes = () => {
         >
           Home
         </Button>
-        <TextField
-          fullWidth
-          sx={{ marginTop: '2%' }}
-          label="Write your note"
-          variant="outlined"
-          multiline
-          rows={10}
-          value={note}
-          onChange={(e) => setNote(e.target.value)}
-        />
-        <Button
-          variant="contained"
-          onClick={handleSave}
-          sx={{ marginTop: '2%' }}
-        >
-          Save
-        </Button>
+        <Grid container>
+          <Grid item xs={4}>
+            <List>
+              {character.notes.map((note, index) => (
+                <ListItem
+                  key={note.id}
+                  onClick={() => handleItemClick(note.content, index)}
+                  sx={{
+                    cursor: 'pointer',
+                    '&:hover': {
+                      backgroundColor: '#f5f5f5',
+                    },
+                  }}
+                >
+                  <ListItemText
+                    primary={character.name}
+                    secondary={note.date}
+                  />
+                </ListItem>
+              ))}
+            </List>
+          </Grid>
+          <Grid item xs={8}>
+            <TextField
+              fullWidth
+              sx={{
+                marginTop: '2%',
+              }}
+              label="Write your note"
+              variant="outlined"
+              multiline
+              rows={10}
+              value={note}
+              onChange={(e) => setNote(e.target.value)}
+            />
+            <Button
+              variant="contained"
+              onClick={handleSave}
+              sx={{ marginTop: '2%' }}
+            >
+              Save
+            </Button>
+          </Grid>
+        </Grid>
       </Container>
     </>
   );
