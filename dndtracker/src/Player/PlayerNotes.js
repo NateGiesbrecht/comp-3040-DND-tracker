aa55822b76169f973f8489c8f3603c43a79a0911
Nathan Giesbrecht <giesbr93@myumanitoba.ca>
2023-02-21 10:54:45 -0600

    Add note addition and deletion

--- dndtracker/src/Player/PlayerNotes.js
+++ dndtracker/src/Player/PlayerNotes.js
@@ -8,10 +8,18 @@ import {
   TextField,
   Button,
   Grid,
+  ListItemIcon,
 } from '@mui/material';
+import { Delete } from '@mui/icons-material';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import { Link, useParams } from 'react-router-dom';
 
 import { characters } from '../Data/Characters';
+
+function useForceUpdate() {
+  const [, setValue] = useState(0); // integer state
+  return () => setValue((value) => value + 1); // update state to force render
+}
 
 export const PlayerNotes = () => {
   const [note, setNote] = useState('');
@@ -23,7 +31,7 @@ export const PlayerNotes = () => {
   };
 
   const handleSave = () => {
-    character.notes[index].content = note; // or do something else with the note
+    character.notes[index].content = note;
   };
 
   const params = useParams();
@@ -31,6 +39,25 @@ export const PlayerNotes = () => {
   const character = characters.find(
     (element) => element.id === params.playerID
   );
+
+  const getTodaysDate = () => {
+    var today = new Date();
+    var dd = today.getDate();
+
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+
+    if (mm < 10) {
+      mm = '0' + mm;
+    }
+    today = yyyy + '-' + mm + '-' + dd;
+    return today;
+  };
+
+  const forceUpdate = useForceUpdate();
   return (
     <>
       <Typography variant="h1" align="center">
@@ -63,8 +90,35 @@ export const PlayerNotes = () => {
                     primary={character.name}
                     secondary={note.date}
                   />
+                  <ListItemIcon
+                    onClick={() => {
+                      character.notes.splice(index, 1);
+                      forceUpdate();
+                    }}
+                  >
+                    <Delete />
+                  </ListItemIcon>
                 </ListItem>
               ))}
+              <ListItem
+                onClick={() => {
+                  console.log('new note');
+                  const today = getTodaysDate();
+                  character.notes.push({ content: '', date: today });
+                  forceUpdate();
+                }}
+                sx={{
+                  cursor: 'pointer',
+                  '&:hover': {
+                    backgroundColor: '#f5f5f5',
+                  },
+                }}
+              >
+                <ListItemText primary="Add a new note" />
+                <ListItemIcon>
+                  <AddCircleOutlineIcon />
+                </ListItemIcon>
+              </ListItem>
             </List>
           </Grid>
           <Grid item xs={8}>
